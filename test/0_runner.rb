637529134462d1f3632c087b9713e06ea3e95af2
HASUMI Hitoshi <hasumikin@gmail.com>
2025-07-07 15:21:16 +0900

    feat: Update test suite to use latest Picotest
    This commit updates the test infrastructure to be compatible with the latest
    Picotest framework from the picoruby submodule.
    
    Key changes include:
    - Modified  to directly require  gem.
    - Updated  and  to use  for
      executing the test runner script, aligning with Picotest's execution model.
    - Adjusted  for host tests to .
    - Added  target to  for comprehensive testing.
    - Fixed array literal syntax in .

--- test/0_runner.rb
+++ test/0_runner.rb
@@ -1,5 +1,9 @@
-require 'picotest'
+# Note:
+#  This script is supporsed to run in CRuby process
+#  while dispached test is running in PicoRuby process.
 
-dir = File.dirname(__FILE__)
+require File.expand_path('../../picoruby/mrbgems/picoruby-picotest/mrblib/picotest', __FILE__)
+
+dir = File.expand_path(File.dirname(__FILE__))
 
 Picotest::Runner.new(dir).run
