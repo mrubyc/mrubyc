2c04a0dda8a678fc24280de4d9d045f1b9ae8448
HASUMI Hitoshi <hasumikin@gmail.com>
2025-07-11 11:46:05 +0900

    Fix/CI: Optimize Docker build and correct test runner path
    - Use `git clone --depth 1` in Dockerfile to speed up the build process by fetching only the latest commit.
    - Adjust the `require` path in `test/0_runner.rb` to correctly locate the `picotest` library, resolving a path issue in the test environment.

--- test/0_runner.rb
+++ test/0_runner.rb
@@ -2,7 +2,7 @@
 #  This script is supposed to run in CRuby process
 #  while dispatched test is running in PicoRuby process.
 
-require File.expand_path('../../picoruby/mrbgems/picoruby-picotest/mrblib/picotest', __FILE__)
+require File.expand_path('../../../picoruby/mrbgems/picoruby-picotest/mrblib/picotest', __FILE__)
 
 dir = File.expand_path(File.dirname(__FILE__))
 
