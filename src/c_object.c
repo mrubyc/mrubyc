a94f4b3dbb085696b25d5c86ab787bf80d0b1b43
HirohitoHigashi <higashi@s-itoc.jp>
2025-08-28 18:26:39 +0900

    small update.

--- src/c_object.c
+++ src/c_object.c
@@ -598,7 +598,7 @@ static void c_object_include(struct VM *vm, mrbc_value v[], int argc)
 //================================================================
 /*! (class method) constants
  */
-static void c_object_constants(mrb_vm *vm, mrb_value v[], int argc)
+static void c_object_constants(mrbc_vm *vm, mrbc_value v[], int argc)
 {
   if( mrbc_type(v[0]) != MRBC_TT_CLASS ) {
     mrbc_raise(vm, MRBC_CLASS(NoMethodError), 0);
