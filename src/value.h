eebb96fc31728ea14ddb8043418e7809e8e9fe31
HirohitoHigashi <higashi@s-itoc.jp>
2025-09-18 11:40:03 +0900

    add boxing_*.h

--- src/value.h
+++ src/value.h
@@ -145,71 +145,18 @@ struct RBasic {
 
 
 //================================================================
-/*!@brief
-  Value object.
-*/
-struct RObject {
-  mrbc_vtype tt : 8;
-  union {
-    mrbc_int_t i;		// MRBC_TT_INTEGER
-#if MRBC_USE_FLOAT
-    mrbc_float_t d;		// MRBC_TT_FLOAT
+#if defined(MRBC_NAN_BOXING)
+# include "boxing_nan.h"
+#else	// !defined(MRBC_NAN_BOXING)
+# include "boxing_no.h"
 #endif
-    mrbc_sym sym_id;		// MRBC_TT_SYMBOL
-    struct RBasic *obj;		// use inc/dec ref only.
-    struct RClass *cls;		// MRBC_TT_CLASS, MRBC_TT_MODULE
-    struct RInstance *instance;	// MRBC_TT_OBJECT
-    struct RProc *proc;		// MRBC_TT_PROC
-    struct RArray *array;	// MRBC_TT_ARRAY
-    struct RString *string;	// MRBC_TT_STRING
-    struct RRange *range;	// MRBC_TT_RANGE
-    struct RHash *hash;		// MRBC_TT_HASH
-    struct RException *exception; // MRBC_TT_EXCEPTION
-    void *handle;		// internal use only.
-  };
-};
+
 typedef struct RObject mrb_object;	// not recommended.
 typedef struct RObject mrb_value;	// not recommended.
 typedef struct RObject mrbc_object;
-typedef struct RObject mrbc_value;
 
 
 /***** Macros ***************************************************************/
-
-// getters
-/*!
-  @def mrbc_type(o)
-  get the type (#mrbc_vtype) from mrbc_value.
-
-  @def mrbc_integer(o)
-  get int value from mrbc_value.
-
-  @def mrbc_float(o)
-  get float(double) value from mrbc_value.
-
-  @def mrbc_symbol(o)
-  get symbol value (#mrbc_sym) from mrbc_value.
-*/
-#define mrbc_type(o)		((o).tt)
-#define mrbc_integer(o)		((o).i)
-#define mrbc_float(o)		((o).d)
-#define mrbc_symbol(o)		((o).sym_id)
-
-
-// make immediate values.
-#define mrbc_integer_value(n)	((mrbc_value){.tt = MRBC_TT_INTEGER, .i=(n)})
-#define mrbc_float_value(vm,n)	((mrbc_value){.tt = MRBC_TT_FLOAT, .d=(n)})
-#define mrbc_nil_value()	((mrbc_value){.tt = MRBC_TT_NIL})
-#define mrbc_true_value()	((mrbc_value){.tt = MRBC_TT_TRUE})
-#define mrbc_false_value()	((mrbc_value){.tt = MRBC_TT_FALSE})
-#define mrbc_bool_value(n)	((mrbc_value){.tt = (n)?MRBC_TT_TRUE:MRBC_TT_FALSE})
-#define mrbc_symbol_value(n)	((mrbc_value){.tt = MRBC_TT_SYMBOL, .sym_id=(n)})
-#define mrbc_immediate_value(...) MRBC_arg_choice(__VA_ARGS__, mrbc_immediate_value2, mrbc_immediate_value1) (__VA_ARGS__)
-#define mrbc_immediate_value1(type) \
-  ((mrbc_value){.tt=type})			// internal use only.
-#define mrbc_immediate_value2(type, v2) \
-  ((mrbc_value){.tt=type, v2})			// internal use only.
-
 
 // (for mruby compatible)
 #define mrb_type(o)		mrbc_type(o)
@@ -576,54 +523,6 @@ int mrbc_arg_b2(struct VM *vm, mrbc_value v[], int argc, int n, int default_valu
 
 
 /***** Inline functions *****************************************************/
-
-//================================================================
-// mrbc_value setters
-
-static inline void mrbc_set_integer(mrbc_value *p, mrbc_int_t n)
-{
-  p->tt = MRBC_TT_INTEGER;
-  p->i = n;
-}
-
-static inline void mrbc_set_float(mrbc_value *p, mrbc_float_t d)
-{
-  p->tt = MRBC_TT_FLOAT;
-  p->d = d;
-}
-
-static inline void mrbc_set_nil(mrbc_value *p)
-{
-  p->tt = MRBC_TT_NIL;
-}
-
-static inline void mrbc_set_true(mrbc_value *p)
-{
-  p->tt = MRBC_TT_TRUE;
-}
-
-static inline void mrbc_set_false(mrbc_value *p)
-{
-  p->tt = MRBC_TT_FALSE;
-}
-
-static inline void mrbc_set_bool(mrbc_value *p, int n)
-{
-  p->tt = n ? MRBC_TT_TRUE: MRBC_TT_FALSE;
-}
-
-static inline void mrbc_set_symbol(mrbc_value *p, mrbc_sym sym_id)
-{
-  p->tt = MRBC_TT_SYMBOL;
-  p->sym_id = sym_id;
-}
-
-static inline void mrbc_set_tt(mrbc_value *p, mrbc_vtype type)	// internal use only.
-{
-  p->tt = type;
-}
-
-
 
 //================================================================
 /*! Increment reference counter
