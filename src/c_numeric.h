536ecb1b3cc9e174518756a360634be041af48d6
HirohitoHigashi <higashi@s-itoc.jp>
2026-02-16 14:51:48 +0900

    Revert c_numeric.h to state before PR #255 for simplicity.

--- src/c_numeric.h
+++ src/c_numeric.h
@@ -15,16 +15,11 @@
 #ifndef MRBC_SRC_C_NUMERIC_H_
 #define MRBC_SRC_C_NUMERIC_H_
 
-#include "vm_config.h"
-#include "value.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if MRBC_USE_STRING_UTF8
-int mrbc_utf8_encode(mrbc_int_t codepoint, char *buf);
-#endif
 
 #ifdef __cplusplus
 }
